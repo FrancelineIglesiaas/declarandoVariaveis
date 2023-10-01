95a7ba895e4841b854c7f1651f155e61d7ad8cc2
Franceline Iglesias <145945337+FrancelineIglesiaas@users.noreply.github.com>
2023-10-01 18:07:47 -0300

    Update conversoes.js

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+//saber o tipo de dados //
+//booleanos // 
+
+//conversao implicita
+const numero = 456;
+const numeroString = "456" 
+
+console.log(numero == numeroString); // ==  conversão implicita - o que esta dentro(valor) - converte em string// 
+
+//conversao explicita //
+//const numeroString = "456" // Number('456')
+//Number()
+//String()
+
+console.log(numero + Number(numeroString));
+//converter numeros com algarismos = NAN - '456a'//
